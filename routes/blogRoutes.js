1a2fe4257ef83be2c018dcdd7b0a2e050825701a
Thura Htet Aung <thurahtetaung711@gmail.com>
2022-05-25 22:09:46 +0630

    Refactoring with MVC approach

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,40 +1,22 @@
 const express = require('express');
-const Blog = require('../models/blog');
+const blogController = require('../controllers/blogController');
+
 const router = express.Router();
 
 // blog routes
-router.get('/blogs', (req, res) => {
-    Blog.find().sort({ createdAt: -1 })
-        .then(result => {
-            res.render('index', { blogs: result, title: 'All blogs' });
-        })
-        .catch(err => {
-            console.log(err);
-        });
-});
+router.get('/', blogController.blog_index);
 
 // create a new blog from user input
-router.post('/blogs', (req, res) => {
-    const blog = new Blog(req.body);
-    blog.save()
-        .then(result => res.redirect('/blogs'))
-        .catch(err => console.log(err));
-}
+router.post('/', blogController.blog_create_post
 );
 
-// view a single blog
-router.get('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findById(id)
-        .then(result => {
-            res.render('details', { blog: result, title: 'Blog Details' });
-        })
-        .catch(err => console.log(err));
-})
-
 // render the create blog form
-router.get('/create', (req, res) => {
-    res.render('create', { title: 'Create a new blog' });
-});
+router.get('/create', blogController.blog_create_get);
+
+// view a single blog
+router.get('/:id', blogController.blog_details)
+
+// delete a blog from database
+router.delete('/:id', blogController.blog_delete)
 
 module.exports = router;
